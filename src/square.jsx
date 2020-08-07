58914998e6160d4126b2686818859fe854e49a99
Rajani Raju <rajaniraju2003@gmail.com>
2020-08-06 22:58:11 -0500

    each button works

--- src/square.jsx
+++ src/square.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Square extends React.Component {
-  
+  clicked = false;
+
+  whenClick = () => {
+    if (this.clicked === false) {
+      this.clicked = true;
+      this.props.onClick();
+    }
+  };
+
   render() {
     return (
-      <button type="button" className="btn" onClick={this.props.onClick} >  </button>
+      <button type="button" className="btn" onClick={this.whenClick}>
+        {" "}
+      </button>
     );
   }
 }
+
 export default Square;
