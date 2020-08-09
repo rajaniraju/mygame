a6ae53720503f0789020b06ab2cc9abc45c242d8
Rajani Raju <rajaniraju2003@gmail.com>
2020-08-08 23:18:29 -0500

    Rw and column passed on a function

--- src/square.jsx
+++ src/square.jsx
@@ -3,18 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class Square extends React.Component {
   clicked = false;
+  text = "";
 
   whenClick = () => {
     if (this.clicked === false) {
+      this.props.onClick(this.props.row, this.props.col);
       this.clicked = true;
-      this.props.onClick();
     }
+    //let indexArray = this.props.index;
+    //console.log(indexArray.index);
   };
 
   render() {
+     if (this.text.length <= 0) {
+     this.text = this.props.text;
+     }
     return (
       <button type="button" className="btn" onClick={this.whenClick}>
-        {this.props.text}
+        {this.text}
       </button>
     );
   }
